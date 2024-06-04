04b424ac4067e2d48295f00a1e3cf312f2554843
rologos <170283129+rologos@users.noreply.github.com>
2024-06-03 21:40:45 -0500

    add pause/play button

--- app.js
+++ app.js
@@ -4,3 +4,14 @@
 
 const btn = document.querySelector('.switch-btn');
 const video = document.querySelector('.video-container');
+
+btn.addEventListener('click', function () {
+
+    if(!btn.classList.contains('slide')) {
+        btn.classList.add('slide')
+        video.pause();
+    } else {
+        btn.classList.remove('slide');
+        video.play();
+    }
+})
